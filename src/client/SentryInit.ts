4e3870d33488c8514f3d01163e093ae2c0d2af34
Mark Dolbyrev <ruflashist@gmail.com>
2026-03-13 10:20:07 +0300

    Changing sample rates for Sentry backup project

--- src/client/SentryInit.ts
+++ src/client/SentryInit.ts
@@ -6,6 +6,9 @@ if (process.env.DEPLOY_ENV !== "dev") {
         // dsn: "https://9ca153276bb034baad9afdb5d1ef2851@o4504694679535616.ingest.us.sentry.io/4510965161066496",
         // Sentry-backup
         dsn: "https://ba75b6bed71331c2b28d5ab13a28ea2d@o4511015108345856.ingest.de.sentry.io/4511015117783120",
+        // Sample rate enabled for the backup project, to make sure we don't hit the limits again
+        sampleRate: 0.1,
+
         environment: process.env.DEPLOY_ENV ?? "prod",
         release: process.env.GIT_COMMIT ?? "unknown",
     });
