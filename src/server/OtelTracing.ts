2d4f79f8db90715cd40a7343cd98b46a43d129a6
Mark Dolbyrev <ruflashist@gmail.com>
2026-04-07 17:26:07 +0300

    Claude: bugfix

--- src/server/OtelTracing.ts
+++ src/server/OtelTracing.ts
@@ -3,6 +3,7 @@ import {
   BasicTracerProvider,
 } from "@opentelemetry/sdk-trace-base";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { trace } from "@opentelemetry/api";
 import * as dotenv from "dotenv";
 import { getServerConfigFromServer } from "../core/configuration/ConfigLoader";
 import { getOtelResource } from "./OtelResource";
@@ -27,5 +28,5 @@ export function initOtelTracing(): void {
     spanProcessors: [new BatchSpanProcessor(traceExporter)],
   });
 
-  provider.register();
+  trace.setGlobalTracerProvider(provider);
 }
